0a4a11442d1939db19785d06028e6ccd1c50752e
Aditya Samant <adityasamant2505@gmail.com>
2022-02-07 08:32:30 -0700

    removed API keys

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -2,8 +2,7 @@
 import axios from "axios";
 
 //pleace your YouTube API key here:
-//const API_KEY = "AIzaSyDFQGY6MiP9IkYqG6k374fTbXxC09aS7YY";
-const API_KEY = "AIzaSyCBwUJOHAQxfdvTnGkbspz3RCi6tus-yYo";
+const API_KEY = "YOUR_API_KEY";
 const CHANNEL_ID = "UCL03ygcTgIbe36o2Z7sReuQ";
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3/",
