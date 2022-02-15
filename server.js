187f02add59b526f700f45d0d5354be777489c08
Aditya Samant <adityasamant2505@gmail.com>
2022-02-14 22:26:42 -0700

    Added winston logging

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+
+//imports
 require('dotenv').config({path:'./.env'});
-const axios = require('axios'); 
+const axios = require('axios'); //to fetch google api calls
 const express = require('express');
-const compression= require('compression');
-const helmet = require('helmet');
-const port = process.env.PORT,
-app = express()
+const compression= require('compression'); //to improve performance
+const helmet = require('helmet'); //for better security
+const logger = require('./logger');
+const app = express();
+const port = process.env.PORT;
 
 const API_URL = `https://www.googleapis.com/youtube/v3/search`;
 const API_KEY = process.env.API_KEY;//"AIzaSyDexPeBv0lqnTP9YMl_38y9HXMHDoaIvcg";
 const CHANNEL_ID = process.env.CHANNEL_ID; //"UCL03ygcTgIbe36o2Z7sReuQ";
 
 
-//To improve app performance
+//Instantiations
+
+
 app.use(helmet());
 app.use(compression());
 async function getVideos(queryString, nextToken){
@@ -22,7 +27,7 @@ async function getVideos(queryString, nextToken){
             channelId: CHANNEL_ID,
             part: "snippet",
             maxResults: 48,
-            key: API_KEY,
+            key: "300 Spartans",
             q: queryString
         }}).then((response) =>{
             returnArg = response.data;
@@ -58,20 +63,24 @@ async function getVideos(queryString, nextToken){
 
 //Get all Covid related videos 
 app.get('/api/getCovidVideos', async(req, res) =>{
-    //res.setHeader('Content-Type', 'application/json');
+
     let items = [];
     const query = "COVID-19 Vaccine Podcast";
+
+    logger.info('Starting API call to get COVID Videos from channel.');
     let response = await getVideos(query);
     if("code" in response){
+       logger.error(`COVID VIDEOS: ${response["code"]}:${response['message']}-Method:${req.method}`);
        res.status(response["code"]).send(response["message"]);
        res.end();
     }
     else{
         items = response.items;
         while('nextPageToken' in response){
+            logger.info(`Sending additional API call to get next page covid videos. Token:${response['nextPageToken']}.`);
             response = await getVideos(query, response['nextPageToken']);
             if("code" in response){
-                
+                logger.error(`COVID VIDEOS: ${response["code"]}:${response["message"]}-Method:{req.method}`);
                 break;
             }
             else{
@@ -83,6 +92,7 @@ app.get('/api/getCovidVideos', async(req, res) =>{
             res.status(response["code"]).send(response["message"]);
             res.end()
         }else{
+            logger.info('Successfully sending covid data back to client');
             res.json(items);
             res.end();
         }
@@ -94,16 +104,21 @@ app.get('/api/getCovidVideos', async(req, res) =>{
 app.get('/api/getOtherVideos', async(req, res) =>{
     let items = [];
     const query = "-COVID -19 -Vaccine -Podcast";
+
+    logger.info('Starting API call to get all non-COVID videos from channel.');
     let response = await getVideos(query);
      if("code" in response){
+       logger.error(`OTHER VIDEOS: ${response["code"]}:${response['message']}-Method:${req.method}`);
        res.status(response["code"]).send(response['message'])
        res.end();
     }
     else{
         items = response.items;
         while('nextPageToken' in response){
+             logger.info(`Sending additional API call to get next page non-COVID videos. Token:${response['nextPageToken']}.`);
             response = await getVideos(query, response['nextPageToken']);
              if("code" in response){
+                logger.error(`OTHER VIDEOS: ${response["code"]}:${response['message']}-Method:${req.method}`);
                 break;
             }
             else{
@@ -115,6 +130,7 @@ app.get('/api/getOtherVideos', async(req, res) =>{
              res.end()
         }
         else{
+            logger.info('Successfully sending covid data back to client');
             res.json(items);
             res.end();
         }
